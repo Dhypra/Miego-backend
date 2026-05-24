b938eb2b25b2444c954e1cb8f67ea86314c3448f
Dhypra <andhikawahyupratamasmart@gmail.com>
2026-05-24 18:37:07 +0700

    fix: PORT

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`\n🍜 MieGo Backend berjalan!`);
   console.log(`   URL  : http://localhost:${PORT}`);
